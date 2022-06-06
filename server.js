513d8ab21586c4e7e347f93670d11f4abdb3e32c
analizajb <96861913+analizajb@users.noreply.github.com>
2022-06-05 23:11:03 -0700

    adding get routes for notes

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
-app.get('./notes', (req, res) => {
-    console.log(`Get notes!`);
+// Storage and retrieval of notes
+const allNotes = require('./db/db.json');
+
+// Read the db.json file and return all saved notes
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
 });
+
+// To join paths
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// TO DO 
+// POST /api/notes should receive a new note to save 
+//on the request body, add it to the db.json file, 
+//and then return the new note to the client. 
+//You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
